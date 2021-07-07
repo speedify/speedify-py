af3d79f3f9642c38eb0e0092e77c5271e3df4858
agizis <agizis@connectify.me>
2021-07-06 20:14:26 -0400

    try catcch

--- sample/monitor.py
+++ sample/monitor.py
@@ -106,48 +106,53 @@ def main():
                          # Prints the current time
             time.strftime('%Y-%m-%d %H:%M:%S',time.localtime(time.time()))
                          # Obtain information CPU
-            stats = speedify.stats(1)
-            active_streams = False
-            for json_array in stats:
-                #print("object type: " + str(json_array[0]))
-                if(str(json_array[0]) == "streaming_stats"):
-                    streaming_right_now = False
-                    #print("  IS streaming_stats")
-                    #print("Item: " + str(json_array))
-                    json_dict = json_array[1]
-                    #print("json_dict" + str(json_dict))
-                    streams_array = json_dict["streams"]
-                    for stream in streams_array :
-                        if stream["active"] == True:
-                            active_streams = True;
-                            print("active stream")
-                            streaming_right_now = True
-                    if( streaming_right_now and not is_streaming):
-                        toaster.show_toast("Live Stream","Speedify is enhancing streaming",icon_path="SpeedifyApp.ico" )
-                    if( not streaming_right_now and is_streaming ):
-                        toaster.show_toast("Stream Complete","Stream complete",icon_path="SpeedifyApp.ico" )
-                    is_streaming = streaming_right_now
-                if(str(json_array[0]) == "state"):
-                    state_obj = json_array[1]
-                    new_state = state_obj["state"]
-                    if (new_state == "DISCONNECTED" or new_state == "DISCONNECTING") and is_streaming == True:
-                        # stats returns things in whatever order it wants so there's no guarantee this will happen in this order to get caught
-                        toaster.show_toast("Stream broke?!" , "Speedify disconnected during live stream",icon_path="SpeedifyApp.ico")
-                        is_streaming = False
-                    if new_state != last_state and new_state == "CONNECTED":
-                        toaster.show_toast("Speedify Connected" , "Ready to start streaming",icon_path="SpeedifyApp.ico")
-                        last_state = new_state
-                    if (new_state == "DISCONNECTED" or new_state == "LOGGED_OUT") and (last_state != "LOGGED_OUT" and last_state != "LOGGED_IN" and last_state != "LOGGING_IN") :
-                        toaster.show_toast("Speedify Disconnected" , "Connect before streaming",icon_path="SpeedifyApp.ico")
 
-                    last_state = new_state
-            if(active_streams):
-                cpu_info = get_cpu_info()
-                             # Obtain information GPU
-                gpu_info = get_gpu_info()
-                             # Added gap
-                print(cpu_info)
-                print(gpu_info,'\n')
+            try:             
+                stats = speedify.stats(1)
+
+                active_streams = False
+                for json_array in stats:
+                    #print("object type: " + str(json_array[0]))
+                    if(str(json_array[0]) == "streaming_stats"):
+                        streaming_right_now = False
+                        #print("  IS streaming_stats")
+                        #print("Item: " + str(json_array))
+                        json_dict = json_array[1]
+                        #print("json_dict" + str(json_dict))
+                        streams_array = json_dict["streams"]
+                        for stream in streams_array :
+                            if stream["active"] == True:
+                                active_streams = True;
+                                print("active stream")
+                                streaming_right_now = True
+                        if( streaming_right_now and not is_streaming):
+                            toaster.show_toast("Live Stream","Speedify is enhancing streaming",icon_path="SpeedifyApp.ico" )
+                        if( not streaming_right_now and is_streaming ):
+                            toaster.show_toast("Stream Complete","Stream complete",icon_path="SpeedifyApp.ico" )
+                        is_streaming = streaming_right_now
+                    if(str(json_array[0]) == "state"):
+                        state_obj = json_array[1]
+                        new_state = state_obj["state"]
+                        if (new_state == "DISCONNECTED" or new_state == "DISCONNECTING") and is_streaming == True:
+                            # stats returns things in whatever order it wants so there's no guarantee this will happen in this order to get caught
+                            toaster.show_toast("Stream broke?!" , "Speedify disconnected during live stream",icon_path="SpeedifyApp.ico")
+                            is_streaming = False
+                        if new_state != last_state and new_state == "CONNECTED":
+                            toaster.show_toast("Speedify Connected" , "Ready to start streaming",icon_path="SpeedifyApp.ico")
+                            last_state = new_state
+                        if (new_state == "DISCONNECTED" or new_state == "LOGGED_OUT") and (last_state != "LOGGED_OUT" and last_state != "LOGGED_IN" and last_state != "LOGGING_IN") :
+                            toaster.show_toast("Speedify Disconnected" , "Connect before streaming",icon_path="SpeedifyApp.ico")
+
+                        last_state = new_state
+                if(active_streams):
+                    cpu_info = get_cpu_info()
+                                 # Obtain information GPU
+                    gpu_info = get_gpu_info()
+                                 # Added gap
+                    print(cpu_info)
+                    print(gpu_info,'\n')
+            except speedify.SpeedifyError as sapie:
+                print("SpeedifyError " + str(sapie))
             time.sleep(delay)
             times += 1
         else:
