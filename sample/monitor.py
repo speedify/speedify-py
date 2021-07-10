6f1c0ace25517a7542abe0b750a09de55219ef2e
agizis <agizis@connectify.me>
2021-07-10 19:52:18 -0400

    go forever!  check every 5 seconds.

--- sample/monitor.py
+++ sample/monitor.py
@@ -22,8 +22,7 @@ from speedify import State, SpeedifyError, Priority
 
 # If you don't see the notifications, it probably means you have Windows Focus Assist on.  Turn it off.
 
-stopped_num = 10000000 # (set a maximum number of acquisition, to prevent explosions recorded text)
-delay = 10 # sampling interval information
+delay = 5 # sampling interval information
 
 toaster = ToastNotifier()
 
@@ -35,6 +34,9 @@ is_streaming = False
 def notify(title, msg):
     global toaster
     try:
+        # using win10toast... annoytingly does not have a silent option, though
+        # the stackoverflow question above explains how to patch it, i don't care enough
+        # for a throw away prototype
         toaster.show_toast(str(title),str(msg),icon_path="SpeedifyApp.ico")
         print("Toast: " +str(title) + " / " + str(msg))
     except Exception as e:
@@ -76,7 +78,7 @@ def get_cpu_info():
         notify("CPUs Recovered", "No CPU Cores fully loaded")
     busy_cpus = busy_cpus_now
 
-    # meh, load is in a future release that pip doesn't have yet
+    # meh, load is in a future release of psutil that pip doesn't have yet
     #load = psutil.getloadavg()
     #print("load: " + str(loads))
     return memtotal, memfree, memused, mempercent, cpu
@@ -89,65 +91,63 @@ def main():
     stream_name = None
     last_state = "DISCONNECTED"
     while True:
-                 # The maximum number of cycles
-        if times < stopped_num:
-                         # Prints the current time
-            time.strftime('%Y-%m-%d %H:%M:%S',time.localtime(time.time()))
-                         # Obtain information CPU
 
-            try:
-                stats = speedify.stats(1)
+                     # Prints the current time
+        time.strftime('%Y-%m-%d %H:%M:%S',time.localtime(time.time()))
+                     # Obtain information CPU
 
-                active_streams = False
-                for json_array in stats:
-                    #print("object type: " + str(json_array[0]))
-                    if(str(json_array[0]) == "streaming_stats"):
-                        streaming_right_now = False
-                        #print("  IS streaming_stats")
-                        #print("Item: " + str(json_array))
-                        json_dict = json_array[1]
-                        #print("json_dict" + str(json_dict))
-                        streams_array = json_dict["streams"]
-                        for stream in streams_array :
-                            if stream["active"] == True:
-                                active_streams = True;
-                                if "name" in stream:
-                                    # lazy cheat.  if there's more than one stream, i'm
-                                    # going to end out just using the name of hte last
-                                    # one which is not really correct, but i'm just
-                                    # experimenting
-                                    app_name = stream["name"]
-                                print("active stream")
-                                streaming_right_now = True
-                        if( streaming_right_now and not is_streaming):
-                            notify("Live Stream","Speedify is enhancing " + app_name )
-                        if( not streaming_right_now and is_streaming ):
-                            notify("Stream Complete",app_name + " stream complete")
-                        is_streaming = streaming_right_now
-                    if(str(json_array[0]) == "state"):
-                        state_obj = json_array[1]
-                        new_state = state_obj["state"]
-                        if (new_state == "DISCONNECTED" or new_state == "DISCONNECTING") and is_streaming == True:
-                            # stats returns things in whatever order it wants so there's no guarantee this will happen in this order to get caught
-                            notify("Stream broke?!" , "Speedify disconnected during live stream")
-                            is_streaming = False
-                        if new_state != last_state and new_state == "CONNECTED":
-                            notify("Speedify Connected" , "Ready to start streaming")
-                            last_state = new_state
-                        if (new_state == "DISCONNECTED" or new_state == "LOGGED_OUT") and (last_state != "LOGGED_OUT" and last_state != "LOGGED_IN" and last_state != "LOGGING_IN") :
-                            notify("Speedify Disconnected" , "Connect before streaming")
+        try:
+            stats = speedify.stats(1)
+
+            active_streams = False
+            for json_array in stats:
+                #print("object type: " + str(json_array[0]))
+                if(str(json_array[0]) == "streaming_stats"):
+                    streaming_right_now = False
+                    #print("  IS streaming_stats")
+                    #print("Item: " + str(json_array))
+                    json_dict = json_array[1]
+                    #print("json_dict" + str(json_dict))
+                    streams_array = json_dict["streams"]
+                    for stream in streams_array :
+                        if stream["active"] == True:
+                            active_streams = True;
+                            if "name" in stream:
+                                # lazy cheat.  if there's more than one stream, i'm
+                                # going to end out just using the name of hte last
+                                # one which is not really correct, but i'm just
+                                # experimenting
+                                app_name = stream["name"]
+                            print("active stream")
+                            streaming_right_now = True
+                    if( streaming_right_now and not is_streaming):
+                        notify("Live Stream","Speedify is enhancing " + app_name )
+                    if( not streaming_right_now and is_streaming ):
+                        notify("Stream Complete",app_name + " stream complete")
+                    is_streaming = streaming_right_now
+                if(str(json_array[0]) == "state"):
+                    state_obj = json_array[1]
+                    new_state = state_obj["state"]
+                    if (new_state == "DISCONNECTED" or new_state == "DISCONNECTING") and is_streaming == True:
+                        # stats returns things in whatever order it wants so there's no guarantee this will happen in this order to get caught
+                        notify("Stream broke?!" , "Speedify disconnected during live stream")
+                        is_streaming = False
+                    elif ((new_state!= "CONNECTED") and last_state == "CONNECTED" ):
+                        notify("Speedify Disconnected" , "Connect before streaming")
+                    elif new_state != last_state and new_state == "CONNECTED":
+                        notify("Speedify Connected" , "Ready to start streaming")
                         last_state = new_state
-                if(active_streams):
-                    cpu_info = get_cpu_info()
+                    last_state = new_state
+            if(active_streams):
+                # only notify about busy cpu / memory if we think you're live streaming.  otherwise it wouldn't be speedify's business
+                cpu_info = get_cpu_info()
+                print(cpu_info)
 
-                    print(cpu_info)
+        except speedify.SpeedifyError as sapie:
+            print("SpeedifyError " + str(sapie))
+        time.sleep(delay)
+        times += 1
 
-            except speedify.SpeedifyError as sapie:
-                print("SpeedifyError " + str(sapie))
-            time.sleep(delay)
-            times += 1
-        else:
-            break
 
 
 if __name__ == '__main__':
